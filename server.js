45389dcf214d4cd04ab79c4f36163670ce73a65d
einstenz <33717014+einstenz@users.noreply.github.com>
2019-10-28 21:32:27 +0000

    Returns simple JSON formatted response and uses body-parser packet

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+var ingredients = [
+  {
+    "id":"232kAk",
+    "text": "Eggs",
+  },
+  {
+    "id":"dkP345",
+    "text":"Milk"
+  },
+  {
+    "id":"dkcuu7",
+    "text":"Bacon"
+  },
+  {
+    "id":"73hdy",
+    "text":"Frog Legs"
+  }
+];
 
 app.get('/', function(request, response) {
-  response.send("My first API!");
+  response.send(ingredients);
 });
 
 app.get('/funions', function(request, response) {
