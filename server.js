8bcc880bfce6cf7408a05b00ec3c59abcc2c82c8
einstenz <33717014+einstenz@users.noreply.github.com>
2019-10-28 21:17:52 +0000

    Implemdented simple API that sends a simple response.

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-df
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+  response.send("My first API!");
+});
+
+app.get('/funions', function(request, response) {
+  response.send("Yo wasup");
+});
+
+app.listen(3000, function() {
+  console.log("Firts API running on port 3000!");
+});
